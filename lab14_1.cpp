9fe1bd82d42e9ad7792443dfa3a567f560366ce8
tigerlab <tiggers12@hotmail.com>
2023-01-13 15:35:17 +0700

    ่่่่ssd

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -17,3 +17,23 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+
+void stat(const double x[],int N,double B[]){
+    double A = 0,SD =0,GM =1,HM =0,Max =x[0],Min =x[0];
+    for (int i = 0; i <= N; i++)
+    {
+        A += x[i];
+        SD += pow(x[i],2);
+        GM *= x[i];
+        HM += 1/x[i];
+        if(x[i]>Max) Max=x[i];
+        if(x[i]<Min) Min=x[i];
+    }
+    B[0] = A/N;
+    B[1] = sqrt((SD/N)-pow(B[0],2));
+    B[2] = pow(GM,1.0/N);
+    B[3] = N/HM;
+    B[4] = Max;
+    B[5] = Min;
+    
+}
